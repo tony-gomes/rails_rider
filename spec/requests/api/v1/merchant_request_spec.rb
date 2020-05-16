c22d4fd1b418dc0fe61a8373245fb247a35228b3
Anthony Gomes <code@tonygom.es>
2020-05-16 09:42:33 -0600

    Update requests spec to check Merchant ID uniqueness

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -5,18 +5,20 @@ describe 'Merchant Resources' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
-    expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
+    expect(response).to be_successful
     expect(merchants.count).to eql(3)
 
     expect(merchants.first).to be_kind_of(Hash)
     expect(merchants.first.count).to eql(4)
+    expect(merchants.first[:id]).not_to eql(merchants.last[:id])
     expect(merchants.first[:id].length).to eql(36)
     expect(merchants.first[:name]).not_to be_empty
 
     expect(merchants.last).to be_kind_of(Hash)
+    expect(merchants.last[:id]).not_to eql(merchants.first[:id])
     expect(merchants.last.count).to eql(4)
     expect(merchants.last[:id].length).to eql(36)
     expect(merchants.last[:name]).not_to be_empty
