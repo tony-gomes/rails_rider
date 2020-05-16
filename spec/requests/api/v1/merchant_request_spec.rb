7999355a1114e0029074ef2b96714b017ba4fc5f
Anthony Gomes <code@tonygom.es>
2020-05-16 08:26:24 -0600

    Add Merchant Resources Request Spec

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Merchant Resources' do
+  it 'Merchant Index' do
+    create_list(:merchant, 3)
+
+    get 'api/v1/merchants'
+
+    expect(response).to be_successful
+  end
+end
