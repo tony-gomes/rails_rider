a476d560f33473779eca32f914e0754bb26f0574
Anthony Gomes <code@tonygom.es>
2020-05-16 09:35:02 -0600

    Add Merchant model specs

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Merchant exists' do
+    merchant1 = create(:merchant)
+
+    expect(merchant1).to be_kind_of(Merchant)
+    expect(merchant1[:id].length).to eql(36)
+    expect(merchant1[:name]).not_to be_empty
+    expect(merchant1[:name]).to be_kind_of(String)
+  end
 end
