d716a9cbbd61f7b53bd95de5979bb984663de9f0
Anthony Gomes <code@tonygom.es>
2020-05-16 08:48:53 -0600

    Fix typo in get path causing bad URI error

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -4,7 +4,7 @@ describe 'Merchant Resources' do
   it 'Merchant Index' do
     create_list(:merchant, 3)
 
-    get 'api/v1/merchants'
+    get '/api/v1/merchants'
 
     expect(response).to be_successful
   end
