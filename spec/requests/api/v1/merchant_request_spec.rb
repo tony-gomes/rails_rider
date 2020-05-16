4274a5523a2f3dfd7467383ec57055739217918c
Anthony Gomes <code@tonygom.es>
2020-05-16 09:23:36 -0600

    Update Merchant requests specs

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -5,7 +5,20 @@ describe 'Merchant Resources' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
-
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eql(3)
+
+    expect(merchants.first).to be_kind_of(Hash)
+    expect(merchants.first.count).to eql(4)
+    expect(merchants.first[:id].length).to eql(36)
+    expect(merchants.first[:name]).not_to be_empty
+
+    expect(merchants.last).to be_kind_of(Hash)
+    expect(merchants.last.count).to eql(4)
+    expect(merchants.last[:id].length).to eql(36)
+    expect(merchants.last[:name]).not_to be_empty
   end
 end
