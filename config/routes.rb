a946b350bac922061a4de984804ebfc76e19da6d
Anthony Gomes <code@tonygom.es>
2020-05-16 11:52:44 -0600

    Add Destroy Action to Merchants Resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show, :create, :update]
+      resources :merchants, except: [:new, :edit]
     end
   end
 end
