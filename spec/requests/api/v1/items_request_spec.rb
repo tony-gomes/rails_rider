4af37c71799c192b8a53dd86b29462ff4a4106d6
Anthony Gomes <code@tonygom.es>
2020-05-16 19:48:49 -0600

    Update Items request spec for Show Action

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,11 +18,34 @@ describe 'Items Resource' do
     expect(items.first[:id]).not_to eql(items.last[:id])
     expect(items.first[:id].length).to eql(36)
     expect(items.first[:name]).not_to be_empty
+    expect(items.first[:description]).not_to be_empty
+    expect(items.first[:unit_price]).not_to be_empty
 
     expect(items.last).to be_kind_of(Hash)
     expect(items.last[:id]).not_to eql(items.first[:id])
     expect(items.last.count).to eql(6)
     expect(items.last[:id].length).to eql(36)
     expect(items.last[:name]).not_to be_empty
+    expect(items.last[:description]).not_to be_empty
+    expect(items.last[:unit_price]).not_to be_empty
+
+  end
+
+  it 'Item Show' do
+    id = create(:item).id
+
+    get "/api/v1/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(item[:id]).to eq(id)
+    expect(item[:id]).to be_kind_of(String)
+    expect(item[:id].length).to eql(36)
+    expect(item[:name]).not_to be_empty
+    expect(item[:id].length).to eql(36)
+    expect(item[:name]).not_to be_empty
+    expect(item[:description]).not_to be_empty
+    expect(item[:unit_price]).not_to be_empty
   end
 end
