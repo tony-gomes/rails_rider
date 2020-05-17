29a5d416ad1fc32d38ce1955beca9ffeaa1624d3
Anthony Gomes <code@tonygom.es>
2020-05-16 20:32:45 -0600

    Update Items Resources for Destroy Action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, except: [:new, :edit]
-      resources :items, only: [:index, :show, :create, :update]
+      resources :items, except: [:new, :edit]
     end
   end
 end
