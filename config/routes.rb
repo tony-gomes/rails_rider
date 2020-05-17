803bd20341b26e04a91cd46893255fc1728349ef
Anthony Gomes <code@tonygom.es>
2020-05-16 20:28:25 -0600

    Update Items Resource for Update Action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, except: [:new, :edit]
-      resources :items, only: [:index, :show, :create]
+      resources :items, only: [:index, :show, :create, :update]
     end
   end
 end
