1a8deb120fa237926850b6895422618f8e46c687
Anthony Gomes <code@tonygom.es>
2020-05-18 07:30:34 -0600

    Update Items Route to be a scoped module of Merchant

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, except: [:new, :edit]
-      resources :items, except: [:new, :edit]
+      scope module: 'merchants' do
+        resources :items, except: [:new, :edit]
+      end
     end
   end
 end
