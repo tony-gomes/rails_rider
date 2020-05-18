beb908aaeb71cfa768ec055f0600325cbd784af5
Anthony Gomes <code@tonygom.es>
2020-05-18 08:33:20 -0600

    Add route to Merchants controller to all_items action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, except: [:new, :edit]
+      get '/merchants/:id/items', to: 'merchants#all_items'
       scope module: 'merchants' do
         resources :items, except: [:new, :edit]
       end
