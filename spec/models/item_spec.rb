d23f05a2c7f70ec701f3f86893f55fb973e6598a
Anthony Gomes <code@tonygom.es>
2020-05-18 07:35:57 -0600

    Update Items model specs for Merchant relationship

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   it 'Item exists' do
-    item1 = create(:item)
+    merchant1 = create(:merchant)
+    item1 = create(:item, merchant_id: merchant1.id)
 
     expect(item1).to be_kind_of(Item)
     expect(item1[:id].length).to eql(36)
+    expect(item1[:merchant_id]).not_to be_empty
+    expect(item1[:merchant_id].length).to eql(36)
     expect(item1[:name]).not_to be_empty
     expect(item1[:name]).to be_kind_of(String)
     expect(item1[:description]).to be_kind_of(String)
