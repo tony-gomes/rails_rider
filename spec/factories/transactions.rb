dc88b46f9b1ebc8f3809ef092e138f26d4f44223
Anthony Gomes <code@tonygom.es>
2020-05-20 17:43:31 -0600

    Update Transaction factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :transaction do
-    association :invoices
+    association :invoice
 
     credit_card_number { "MyString" }
-    credit_card_expiration { "MyString" }
+    credit_card_expiration_date { "MyString" }
+    result { "MyString" }
   end
 end
